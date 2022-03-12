16b78d9606ef11031050a9f54c36688ca57fa109
david <davidwang7777@gmail.com>
2022-03-12 10:20:26 +0800

    助教建議1

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/vue-gh-pages/' //儲存庫名稱
+//       : '/'
+// }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/Vue-homework6/' : '/'
+}
